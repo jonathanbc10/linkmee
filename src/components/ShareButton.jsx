5126406245ed24db0c98662c30ed4dab9f1ea569
Jonathan B <jonathanbarillas@protonmail.com>
2023-06-22 02:00:23 -0400

    Share icon replaced by copy-to-clipboard icon. Also, copy function fixed to work on mobile devices

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -2,26 +2,38 @@ import PropTypes from 'prop-types'
 import { ShareIcon } from '../assets'
 
 const ShareButton = ({ classN, iconFill, link }) => {
-    const defaultClasses = 'cursor-pointer transition duration-200 transform hover:scale-105 w-[40px] h-[40px] rounded-full bg-gray-200'
+  const defaultClasses = 'cursor-pointer transition duration-200 transform hover:scale-105 w-[40px] h-[40px] rounded-full bg-gray-200'
 
-    const handleClick = async (e) => {
-        e.preventDefault();
-        const shareLink = link || window.location.href;
-        await navigator.clipboard.writeText(shareLink);
-        window.alert('Link copied to clipboard!');
-    };
+  const handleCopy = (e) => {
+    e.preventDefault()
+    const shareLink = link || window.location.href;
 
-    return (
-        <div className={classN ? classN : defaultClasses} onClick={handleClick}>
-            <ShareIcon fill={iconFill ? iconFill : 'black'} />
-        </div>
-    )
+    const input = document.createElement('input');
+    input.style.position = 'fixed';
+    input.style.opacity = 0;
+    input.value = shareLink;
+    document.body.appendChild(input);
+
+    input.select();
+    input.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+
+    document.body.removeChild(input);
+
+    window.alert('Link copied to clipboard!');
+  };
+
+  return (
+    <button className={classN ? classN : defaultClasses} onClick={handleCopy}>
+      <ShareIcon fill={iconFill ? iconFill : 'black'} />
+    </button>
+  )
 }
 
 ShareButton.propTypes = {
-    classN: PropTypes.string,
-    iconFill: PropTypes.string,
-    link: PropTypes.string,
+  classN: PropTypes.string,
+  iconFill: PropTypes.string,
+  link: PropTypes.string,
 }
 
 export default ShareButton
