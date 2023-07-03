71f60572b73213d27dfb294d8132ce572d1048bc
Jonathan B <jonathanbarillas@protonmail.com>
2023-07-02 20:01:45 -0400

    LinkButton component style fixed to perfect center elements

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -7,13 +7,13 @@ const LinkButton = ({ name, link, icon }) => {
         <a
             href={link}
             target='_blank'
-            className='w-[100%] h-14 bg-[#252525] m-2 
+            className='w-[100%] h-14 bg-[#252525] my-2 
             rounded-2xl text-white no-underline flex 
             justify-between items-center transition duration-200 transform hover:scale-105 hover:bg-gradient-to-r from-purple-500 via-purple-600 to-purple-500'
             rel="noreferrer"
         >
             <div className=''>
-                {IconComponent && <IconComponent className='w-9 my-1 mx-2' />}
+                {IconComponent && <IconComponent className='w-9 mx-2' />}
             </div>
             <p>{name}</p>
             <ShareButton link={link} classN='m-2 w-10 h-10 rounded-full bg-[#343434]' iconFill={'#a0a0a0'} />
