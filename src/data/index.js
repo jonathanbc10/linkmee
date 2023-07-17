ea94b8742f4e856bae8037cdbd32ac2f8a85b9ad
Jonathan B <jonathanbarillas@protonmail.com>
2023-07-17 15:44:31 -0400

    New link added with BriefCaseIcon and Animation

--- src/data/index.js
+++ src/data/index.js
@@ -1,43 +1,47 @@
 import { profilepic } from "../assets";
 import {
-    GithubIcon,
-    TwitterIcon,
-    LinkedinIcon,
-    InstagramIcon,
-    WebsiteIcon
+  GithubIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  InstagramIcon,
+  WebsiteIcon,
+  BriefCaseIcon,
 } from "../assets";
 
-export const profileInfo =
-{
-    username: "ArresteDeveloper",
-    pic: profilepic
-}
-
+export const profileInfo = {
+  username: "ArresteDeveloper",
+  pic: profilepic,
+};
 
 export const links = [
-    {
-        name: "Github",
-        link: "https://github.com/jonathanbc10",
-        icon: GithubIcon
-    },
-    {
-        name: "Twitter",
-        link: "https://twitter.com/arresteDevelopr",
-        icon: TwitterIcon
-    },
-    {
-        name: "LinkedIn",
-        link: "https://www.linkedin.com/in/jonathan-bc/",
-        icon: LinkedinIcon
-    },
-    {
-        name: "Instagram",
-        link: "https://www.instagram.com/arrested_develop3r/",
-        icon: InstagramIcon
-    },
-    {
-        name: "Best link EVER!",
-        link: "https://jsninja.netlify.app",
-        icon: WebsiteIcon
-    }
-]
+  {
+    name: "Click for hire!",
+    link: "https://www.workana.com/freelancer/3e74b4e42429686a0719adce030303aa",
+    icon: BriefCaseIcon,
+  },
+  {
+    name: "Github",
+    link: "https://github.com/jonathanbc10",
+    icon: GithubIcon,
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/arresteDevelopr",
+    icon: TwitterIcon,
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/jonathan-bc/",
+    icon: LinkedinIcon,
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/arrested_develop3r/",
+    icon: InstagramIcon,
+  },
+  {
+    name: "Best link EVER!",
+    link: "https://jsninja.netlify.app",
+    icon: WebsiteIcon,
+  },
+];
